a485ad0217e222495bc5e2c583a4fa3889be1d55
senya2289 <senya707.azusa@gmail.com>
2025-09-29 14:26:46 +0300

    Add files via upload

---
+++ PR1/PR1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int secretNumber = random.Next(1, 11); // От 1 до 10 включительно
+
+            try
+            {
+                Console.Write("Угадайте число от 1 до 10: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == secretNumber)
+                {
+                    Console.WriteLine("Поздравляем! Вы угадали число.");
+                }
+                else
+                {
+                    Console.WriteLine($"Не угадали. Загаданное число было {secretNumber}.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: Введено не число. Программа завершена.");
+            }
+        }
+    }
+}
---
+++ PR2/PR2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace SortedIndices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание массива
+            Random random = new Random();
+            double[] array = new double[10];
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = random.NextDouble() * 20 - 10; // [-10, 10)
+            }
+
+            // Создание массива индексов, отсортированных по значениям
+            int[] indices = Enumerable.Range(0, 10)
+                .OrderBy(i => array[i])
+                .ToArray();
+
+            // Вывод массивов
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"array[{i}] = {array[i]:F4}");
+            }
+            Console.WriteLine("Массив индексов (по возрастанию значений):");
+            Console.WriteLine(string.Join(" ", indices));
+        }
+    }
+}
