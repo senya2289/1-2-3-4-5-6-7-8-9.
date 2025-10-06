fe99c9b43faa538ad929591ea976d8f92cf211df
senya2289 <senya707.azusa@gmail.com>
2025-10-06 12:06:09 +0300

    Add files via upload

---
+++ М2.4/М2.4/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Задание_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание массива объектов, реализующих интерфейс IDrawable
+            IDrawable[] shapes = new IDrawable[3];
+
+            // Ввод данных для круга
+            Console.Write("Введите радиус круга:");
+            double radius = double.Parse(Console.ReadLine());
+            shapes[0] = new Circle(radius);
+
+            // Ввод данных для прямоугольника
+            Console.Write("Введите ширину прямоугольника:");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Введите высоту прямоугольника:");
+            double height = double.Parse(Console.ReadLine());
+            shapes[1] = new Rectangle(width, height);
+
+            // Ввод данных для треугольника
+            Console.Write("Введите длину первой стороны треугольника:");
+            double sideA = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину второй стороны треугольника:");
+            double sideB = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину третьей стороны треугольника:");
+            double sideC = double.Parse(Console.ReadLine());
+            shapes[2] = new Triangle(sideA, sideB, sideC);
+
+            // Вызов метода Draw для каждого объекта
+            Console.WriteLine("\nРисуем фигуры:");
+            foreach (IDrawable shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+}
---
+++ М2.4/М2.4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Задание_4
+{
+
+    // Определение интерфейса IDrawable
+    interface IDrawable
+    {
+        void Draw();
+    }
+
+    // Класс Circle, реализующий интерфейс IDrawable
+    class Circle : IDrawable
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        // Реализация метода Draw для круга
+        public void Draw()
+        {
+            Console.WriteLine($"Рисуем круг с радиусом: {Radius}");
+        }
+    }
+
+    // Класс Rectangle, реализующий интерфейс IDrawable
+    class Rectangle : IDrawable
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        // Реализация метода Draw для прямоугольника
+        public void Draw()
+        {
+            Console.WriteLine($"Рисуем прямоугольник с шириной: {Width} и высотой: {Height}");
+        }
+    }
+
+    // Класс Triangle, реализующий интерфейс IDrawable
+    class Triangle : IDrawable
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        // Реализация метода Draw для треугольника
+        public void Draw()
+        {
+             Console.WriteLine($"Рисуем треугольник со сторонами: {SideA}, {SideB}, {SideC}");
+        }
+    }
+}
