f676c39f0a000e30ad9a32a3d6f01b9a9ed11e97
senya2289 <senya707.azusa@gmail.com>
2025-10-08 08:35:43 +0300

    Add files via upload

---
+++ Задание 1/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Задание_1
+{
+    // Базовый класс "Фигура"
+    public abstract class Figure
+    {
+        // Метод для вычисления площади (будет переопределён в наследниках)
+        public abstract double CalculateArea();
+    }
+
+    // Производный класс "Круг"
+    public class Circle : Figure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    // Производный класс "Прямоугольник"
+    public class Rectangle : Figure
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    // Производный класс "Треугольник"
+    public class Triangle : Figure
+    {
+        public double Base { get; set; }
+        public double Height { get; set; }
+
+        public Triangle(double baseLength, double height)
+        {
+            Base = baseLength;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * Base * Height;
+        }
+    }
+}
---
+++ Задание 1/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows;
+
+namespace Задание_1
+{
+    public partial class MainWindow : Window
+    {
+        // Делегат для вызова метода вычисления площади
+        private delegate double AreaCalculatorDelegate();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShapeSelector.SelectionChanged += ShapeSelector_SelectionChanged;
+        }
+
+        private void ShapeSelector_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            InputFields.Children.Clear();
+            if (ShapeSelector.SelectedItem is System.Windows.Controls.ComboBoxItem selectedShape)
+            {
+                switch (selectedShape.Content.ToString())
+                {
+                    case "Круг":
+                        AddInputField("Радиус:");
+                        break;
+                    case "Прямоугольник":
+                        AddInputField("Ширина:");
+                        AddInputField("Высота:");
+                        break;
+                    case "Треугольник":
+                        AddInputField("Основание:");
+                        AddInputField("Высота:");
+                        break;
+                }
+            }
+        }
+
+        private void AddInputField(string label)
+        {
+            var stackPanel = new System.Windows.Controls.StackPanel { Orientation = System.Windows.Controls.Orientation.Horizontal, Margin = new Thickness(0, 5, 0, 5) };
+            stackPanel.Children.Add(new System.Windows.Controls.TextBlock { Text = label, Width = 100 });
+            stackPanel.Children.Add(new System.Windows.Controls.TextBox { Width = 100 });
+            InputFields.Children.Add(stackPanel);
+        }
+
+        private void CalculateAreaButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ShapeSelector.SelectedItem is System.Windows.Controls.ComboBoxItem selectedShape)
+            {
+                AreaCalculatorDelegate areaCalculator = null;
+                Figure figure = null;
+
+                try
+                {
+                    switch (selectedShape.Content.ToString())
+                    {
+                        case "Круг":
+                            double radius = double.Parse(((System.Windows.Controls.TextBox)((System.Windows.Controls.StackPanel)InputFields.Children[0]).Children[1]).Text);
+                            figure = new Circle(radius);
+                            break;
+                        case "Прямоугольник":
+                            double width = double.Parse(((System.Windows.Controls.TextBox)((System.Windows.Controls.StackPanel)InputFields.Children[0]).Children[1]).Text);
+                            double height = double.Parse(((System.Windows.Controls.TextBox)((System.Windows.Controls.StackPanel)InputFields.Children[1]).Children[1]).Text);
+                            figure = new Rectangle(width, height);
+                            break;
+                        case "Треугольник":
+                            double baseLength = double.Parse(((System.Windows.Controls.TextBox)((System.Windows.Controls.StackPanel)InputFields.Children[0]).Children[1]).Text);
+                            double triangleHeight = double.Parse(((System.Windows.Controls.TextBox)((System.Windows.Controls.StackPanel)InputFields.Children[1]).Children[1]).Text);
+                            figure = new Triangle(baseLength, triangleHeight);
+                            break;
+                    }
+
+                    if (figure != null)
+                    {
+                        areaCalculator = figure.CalculateArea;
+                        ResultTextBlock.Text = $"Площадь: {areaCalculator()}";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка ввода данных: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
