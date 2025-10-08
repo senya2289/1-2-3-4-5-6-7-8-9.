5a5fe3c997fd597adf08a5f66fd8ac1f65d209ca
senya2289 <senya707.azusa@gmail.com>
2025-10-08 08:41:55 +0300

    Add files via upload

---
+++ Задание 5/Class1.cs
@@ -0,0 +1,53 @@
+﻿namespace Задание_5
+{
+    public class Sorter
+    {
+        // Метод сортировки пузырьком
+        public static void BubbleSort(int[] array)
+        {
+            int length = array.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        // Обмен
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        // Метод быстрой сортировки
+        public static void QuickSort(int[] array, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = array[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (array[i] < pivot) i++;
+                while (array[j] > pivot) j--;
+
+                if (i <= j)
+                {
+                    // Обмен
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            // Рекурсивные вызовы
+            if (left < j)
+                QuickSort(array, left, j);
+            if (i < right)
+                QuickSort(array, i, right);
+        }
+    }
+}
---
+++ Задание 5/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Задание_5
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SortButton_Click(object sender, RoutedEventArgs e)
+        {
+            string input = NumbersTextBox.Text;
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Введите хотя бы одно число.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            // Преобразование строки в массив чисел
+            int[] numbers = input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(n => int.TryParse(n.Trim(), out int num) ? num : 0)
+                                 .ToArray();
+
+            // Проверка на пустые значения
+            if (numbers.Length == 0)
+            {
+                MessageBox.Show("Введите корректные числа.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            // Определение метода сортировки
+            Action<int[]> sortingMethod = null;
+
+            if (SortMethodSelector.SelectedItem is ComboBoxItem selectedMethod)
+            {
+                switch (selectedMethod.Content.ToString())
+                {
+                    case "Сортировка пузырьком":
+                        sortingMethod = Sorter.BubbleSort;
+                        break;
+
+                    case "Быстрая сортировка":
+                        sortingMethod = array => Sorter.QuickSort(array, 0, array.Length - 1);
+                        break;
+                }
+
+                // Сортировка и вывод результата
+                sortingMethod?.Invoke(numbers);
+                ResultTextBox.Text = string.Join(", ", numbers);
+            }
+            else
+            {
+                MessageBox.Show("Выберите метод сортировки.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
